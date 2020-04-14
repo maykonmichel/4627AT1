4ba1ab40850491f4fc9dbc0619944c319801950d
maykon <maykonmichelpalma@gmail.com>
2020-04-14 09:13:08 -0300

    Configured methods paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,14 @@ import React, { memo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from '../components/Header';
+
+import AureaSection from '../pages/AureaSection';
+import Bisection from '../pages/Bisection';
+import DichotomousSearch from '../pages/DichotomousSearch';
+import FibonacciSearch from '../pages/FibonacciSearch';
 import Home from '../pages/Home';
+import Newton from '../pages/Newton';
+import UniformSearch from '../pages/UniformSearch';
 
 const Routes = memo(() => {
   return (
@@ -11,8 +18,26 @@ const Routes = memo(() => {
         <Header />
 
         <Switch>
-          <Route path="/">
+          <Route path="/" exact>
             <Home />
+          </Route>
+          <Route path="/busca-uniforme" exact>
+            <UniformSearch />
+          </Route>
+          <Route path="/busca-dicotomica" exact>
+            <DichotomousSearch />
+          </Route>
+          <Route path="/secao-aurea" exact>
+            <AureaSection />
+          </Route>
+          <Route path="/busca-de-fibonacci" exact>
+            <FibonacciSearch />
+          </Route>
+          <Route path="/bissecao" exact>
+            <Bisection />
+          </Route>
+          <Route path="/newton" exact>
+            <Newton />
           </Route>
         </Switch>
       </div>
