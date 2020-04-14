ad12274dcf9366c4cd8095a4fe7efcd40e1dd1e3
maykon <maykonmichelpalma@gmail.com>
2020-04-14 07:20:19 -0300

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { memo } from 'react';
+import { AppBar, Button, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Header = memo(() => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default Header;
