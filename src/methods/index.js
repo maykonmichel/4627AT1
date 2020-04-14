c49d197d3ef21283facace121d72955d3da68f08
maykon <maykonmichelpalma@gmail.com>
2020-04-14 12:43:02 -0300

    Added standard method function

--- src/methods/index.js
+++ src/methods/index.js
@@ -1,9 +1,19 @@
 import { evaluate } from 'mathjs';
 
+import aureaSection from './aureaSection';
+import bisection from './bisection';
 import dichotomousSearch from './dichotomousSearch';
+import fibonacciSearch from './fibonacciSearch';
+import newton from './newton';
+import uniformSearch from './uniformSearch';
 
 const methods = {
+  aureaSection,
+  bisection,
   dichotomousSearch,
+  fibonacciSearch,
+  newton,
+  uniformSearch,
 };
 
 export default (method, { f, ...args }) =>
