41c4b65acc3e181bb3c26db8548dab1fed3edca8
Jamilly Souza <jamilly.gcsouza@gmail.com>
2020-05-08 23:57:40 -0300

    Inicio Fibonacci

--- src/methods/fibonacciSearch.js
+++ src/methods/fibonacciSearch.js
@@ -1,4 +1,16 @@
-export default async () =>
+export default async ({ f, a, b, e }) =>
   new Promise((resolve, reject) => {
-    return reject(new Error('TODO implement fibonacci search'));
+    let alfa = (-1 + Math.sqrt(5))/2;
+    let beta = 1 - alfa;
+
+    for (let i = 1; b - a >= e && i < 9999; i += 1){
+        const mi = a + beta*(b-a);
+        const lambda = a + alfa*(b-a);
+
+        if(f(mi) > f(lambda)) a = mi;
+        else b = lambda;
+    }
+
+    if (b - a >= e) return reject(new Error('Não foi possível determinar o mínimo da função'));
+    return resolve((b + a) / 2);
   });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,13 @@ import React, { memo } from 'react';
 export default memo(() => {
   return (
     <>
+    <center>
       <h1>Home</h1>
+      <h2>Alunos</h2>
+      <p>Beatriz Tavares</p>
+      <p>Jamilly Souza</p>
+      <p>Maykon Michel</p>
+    </center>
     </>
   );
 });
