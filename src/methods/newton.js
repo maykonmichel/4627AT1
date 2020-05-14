748b69fd74fd1986684240501864c6706a7849b4
Beatriz Tavares Vieira <beatriztavaresvieira@hotmail.com>
2020-05-14 18:26:21 -0300

    Metodo busca uniforme finalizado inicio do Newton

--- src/methods/newton.js
+++ src/methods/newton.js
@@ -1,4 +1,16 @@
-export default async () =>
+import { create, all } from 'mathjs';
+
+export default async ({ f, fn, a, b, e }) =>
   new Promise((resolve, reject) => {
-    return reject(new Error('TODO implement newton'));
+    
+    const config = { };
+    const math = create(all, config);
+    
+    alert(math.derivative(fn,'x').evaluate({x:a}));
+    let xn = a;
+    let lim = b;
+    
+    //if () return reject(new Error('Não foi possível determinar o mínimo da função'));
+    
+    return resolve('a');
   });
