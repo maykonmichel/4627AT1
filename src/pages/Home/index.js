48685fcf138d317709c253c2652365cfcf19a3ef
maykonmichel <maykonmichelpalma@gmail.com>
2020-05-15 10:23:09 -0300

    Added R.A.s

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,25 @@
 import React, { memo } from 'react';
+import { Divider, Grid, Paper } from '@material-ui/core';
+
+import useStyles from '../Newton/useStyles';
 
 export default memo(() => {
+  const classes = useStyles();
+
   return (
-    <>
-    <center>
-      <h1>Home</h1>
-      <h2>Alunos</h2>
-      <p>Beatriz Tavares</p>
-      <p>Jamilly Souza</p>
-      <p>Maykon Michel</p>
-    </center>
-    </>
+    <Grid container justify="center">
+      <Grid item xs={12} lg={5}>
+        <Paper className={[classes.paper, classes.form]}>
+          <h1>Alunos</h1>
+          <p>Beatriz Tavares - 181022011</p>
+          <p>Jamilly Souza - 181020416</p>
+          <p>Maykon Michel - 181022656</p>
+          <Divider />
+          <p>
+            Código fonte em: <a href="https://github.com/maykonmichel/4627AT1">https://github.com/maykonmichel/4627AT1</a>
+          </p>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 });
